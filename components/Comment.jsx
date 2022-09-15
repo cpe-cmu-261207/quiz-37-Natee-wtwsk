6362c234bf262b600b4934d59fe1a41953d2a698
Natee <gg977120@gmail.com>
2022-09-15 14:31:11 +0700

    Test

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -3,11 +3,26 @@ import Reply from "./Reply";
 
 export default function Comment(props) {
   return (
-    <div>
-      {/* Entire Comment div */}
-      <div></div>
-
-      {/*render Reply here... */}
-    </div>
+    <div className="d-flex gap-2 my-2">
+        <img
+          src="/profileImages/lisa.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">Lisa</span>
+          <br />
+          <span>จริงค่า</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span className="text-muted">999 คน</span>
+          </div>
+        </div>
+      </div>
   );
 }
